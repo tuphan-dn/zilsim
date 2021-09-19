1faeb526d2a54e72d6790778f1b1d7cc0b435b43
tuphan-dn <tuphan@descartes.network>
2021-09-19 11:09:37 +0700

    programmable interests

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const B = 5000000000n * decimals
 const interests = (10n * BigInt.PRECISION) / 100n // 4%
 
 const amm = new AMM(A, B, interests)
-const bot = new SwapBot(A / 1000n, B / 1000n, amm)
+const bot = new SwapBot(A / 100n, B / 100n, amm)
 
 const marketTrend = 0.65
-let stop = 10000
+let stop = 100
 console.log(amm.history[amm.history.length - 1])
 while (stop-- > 0) {
   console.log('=======================================================')
